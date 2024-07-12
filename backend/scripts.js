7fa41e384df1e5dc3b208a6bf2c867bd8903f79c
Alan Njogu <njoguu@localhost.localdomain>
2024-07-12 11:49:12 +0300

    chore: remove events section

--- backend/scripts.js
+++ backend/scripts.js
@@ -4,37 +4,11 @@ async function loadData(){
     const response = await fetch(URL);
 
     const data = await response.json();
-    const events = data.Events;
     const resources = data.Resources;
 
-    const eventsContainer = document.getElementById('events-container');
     const resourcesContainer = document.getElementById('resources-container');
 
-    eventsContainer.innerHTML = '';
     resourcesContainer.innerHTML = '';
-
-    if (events.length == 0){
-        eventsContainer.innerHTML += `
-            <div class="text-center">
-                <p>No Events data to show!</p>
-            </div>
-        `
-    }else{
-        for (let event of events) {
-            eventsContainer.innerHTML += `
-              <div class="card w-72 bg-base-100 shadow-xl">
-                <figure><img src="${event.Image}" alt="${event.Name}" /></figure>
-                <div class="card-body">
-                  <h2 class="card-title">${event.Name}</h2>
-                  <p>${event.Location}<br/>@ ${event.Date}</p>
-                  <div class="card-actions justify-end">
-                    <a class="btn bg-[#EF5B25] text-white" href="${event.Link}" target="_blank">View Event</a>
-                  </div>
-                </div>
-              </div>
-            `
-        }
-    }
 
     if (resources.length == 0){
       resourcesContainer.innerHTML += `
